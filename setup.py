f48242255857262ffc19e0974cf7d3da03c87613
J.R. Calzada <jreniel@gmail.com>
2018-09-01 15:12:56 -0400

    --allow-empty-message

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 import setuptools
 
+# import os
+# os.system('pip install cython')
+# os.system('pip install git+https://github.com/jswhit/pyproj.git@master')
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
@@ -7,15 +11,33 @@ setuptools.setup(
     name="AdcircPy",
     version="0.8.0",
     author="Jaime R Calzada",
-    author_email="jaime.calzada@noaa.gov",
+    author_email="jaime.calzada@gmail.com",
     description="Python package for working with ADCIRC input and output files.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jreniel/AdcircPy.git",
-    packages=setuptools.find_packages(),
+    # packages=['matplotlib',
+    #                    'netCDF4',
+    #                    'scipy',
+    #                    'GDAL',
+    #                    'haversine',
+    #                    'cython',
+    #                    'pyproj'],
     classifiers=[
         "Programming Language :: Python :: 3",
         # "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+        "Operating System :: OS Independent",],
+    install_requires=['matplotlib',
+                       'netCDF4',
+                       'scipy',
+                       'GDAL',
+                       'haversine',
+                       'cython',
+                       'wget'],
+    dependency_links=['git+https://github.com/jswhit/pyproj.git@master']
+
+    # extras_require={"extras": [ "Cython==0.25.2", ],},
+    # entry_points={'app_name':["foo = mypackage.some_module:foo",],}
+
+
 )
