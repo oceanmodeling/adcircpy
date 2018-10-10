bebfc9ee3556450a53fc840e767f53911fec3719
J.R. Calzada <jreniel@gmail.com>
2018-10-10 15:38:55 -0400

    Updates to Datum submodule
    Provided methods to build Datum grids using nearest neighbor
    interpolation based on the grid transformations provided by VDatum.

--- AdcircPy/Datum/VDatum.py
+++ AdcircPy/Datum/VDatum.py
@@ -2,15 +2,11 @@ import os
 import subprocess
 import shutil
 import numpy as np
+from AdcircPy.Tides import TidalForcing as _TidalForcing
 
 
-def jar_wrapper(xyz, ihorz, ivert,  ohorz,  overt,  vdatumdir, verbose=True):
-    if os.path.isfile(vdatumdir+"/vdatum.jar")==False:
-        raise FileNotFoundError("vdatum.jar not found in provided path: '{}'".format(vdatumdir))
-
-    try: os.makedirs('vdatum_tmp/input')
-    except: pass
-    
+def jar_wrapper(xyz, ihorz, ivert,  ohorz,  overt,  vdatum_jar_path, verbose=True, return_nodata=False):
+    os.makedirs('vdatum_tmp/input', exist_ok=True)
     np.savetxt('vdatum_tmp/input/vdatum.txt', xyz, fmt='%.18f')
     ihorz = 'ihorz:'+ihorz
     ivert = 'ivert:'+ivert
@@ -21,24 +17,22 @@ def jar_wrapper(xyz, ihorz, ivert,  ohorz,  overt,  vdatumdir, verbose=True):
     Popen_list.append('java')
     # Popen_list.append('-Djava.awt.headless=true')
     Popen_list.append('-jar')
-    Popen_list.append(vdatumdir+"/vdatum.jar")
+    Popen_list.append(vdatum_jar_path)
     Popen_list.append(ihorz)
     Popen_list.append(ivert)
     Popen_list.append(ohorz)
     Popen_list.append(overt)
-    Popen_list.append('-nodata')
+    if return_nodata==False:
+        Popen_list.append('-nodata')
     Popen_list.append("-file:txt:space,0,1,2:vdatum_tmp/input/vdatum.txt:vdatum_tmp/output")
     # Popen_list.append("-file:txt:space,0,1,2:{}/vdatum_tmp/input/vdatum.txt:{}/vdatum_tmp/output".format(os.getcwd(),os.getcwd()))
-
     if verbose==True:
         print(' '.join(Popen_list))
-    
     p = subprocess.Popen(Popen_list, stdout=subprocess.PIPE, bufsize=1)
     if verbose==True:
         for line in iter(p.stdout.readline, b''):
             print(line.decode("utf-8").strip('\n'),)
-    p.terminate()
-
+    p.send_signal(9)
     xyz = np.loadtxt('vdatum_tmp/output/vdatum.txt')
     shutil.rmtree('vdatum_tmp')
     return xyz
