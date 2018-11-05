e1475b4538db344ae05f8aebd7446abbf23c29e4
jreniel <jreniel@gmail.com>
2018-11-05 13:42:51 -0500

    Introduced method for generating storm runs.

--- AdcircPy/Model/MeteorologicalStationsOutput.py
+++ AdcircPy/Model/MeteorologicalStationsOutput.py
@@ -12,21 +12,5 @@ class MeteorologicalStationsOutput(_StationsOutput):
     raise NotImplementedError
 
   @classmethod
-  def from_fort15(cls, path, sampling_frequency=None, netcdf=True, spinup=False, harmonic_analysis=False):
-    stations=dict()
-    with open(path,'r') as f:
-      for line in f:
-        if 'NOUTM' in line:
-          num = int(f.readline().split('!')[0].strip().split(' ')[0])
-          for i in range(num):
-            line = f.readline().split('!')
-            if len(line)>0:
-              station_name = line[1].strip(' \n')
-            else:
-              station_name = str(i)
-            coords = line[0].split(' ')
-            coords = [float(x) for x in coords if x != '']
-            x = float(coords[0])
-            y = float(coords[1])
-            stations[station_name]={'x' : x, 'y' : y}
-    return cls(stations, sampling_frequency, netcdf, spinup, harmonic_analysis)
+  def from_fort15(cls, path, sampling_frequency=None, netcdf=True, spinup=False, harmonic_analysis=False, _hint='NOUTM'):
+    return cls(cls.parse_fort15(path, _hint), sampling_frequency, netcdf, spinup, harmonic_analysis)
