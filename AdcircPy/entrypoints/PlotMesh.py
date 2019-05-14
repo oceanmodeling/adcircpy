25d05585644e3e640cbfd7edc8ae460f01232b14
jreniel <jreniel@gmail.com>
2019-05-14 09:54:39 -0400

    entrypoints are work in progress

--- AdcircPy/entrypoints/PlotMesh.py
+++ AdcircPy/entrypoints/PlotMesh.py
@@ -1,29 +1,35 @@
-#! /usr/bin/env python
-import os
 import argparse
-import matplotlib.pyplot as plt
 from AdcircPy import AdcircPy
 
-class PlotMesh(object):
-  def __init__(self):
-    self.parse_args()
-    self.read_mesh()
-    self.plot_mesh()
 
-  def parse_args(self):
-    self.args = argparse.ArgumentParser(description="Program to see a quick plot of an ADCIRC mesh.")
-    self.args.add_argument("mesh_path",help="ADCIRC mesh file path.")
-    self.args = self.args.parse_args()
-    
-  def read_mesh(self):
-    self.fort14 = AdcircPy.read_mesh(self.args.mesh_path)
-    
-  def plot_mesh(self):
-    self.fort14.make_plot()
-    plt.show()
+class PlotMesh(object):
+    def __init__(self):
+        self.parse_args()
+        self.read_mesh()
+        self.plot_mesh()
+
+    def parse_args(self):
+        parser = argparse.ArgumentParser(
+                        description="Program to see a quick plot of an "
+                                    + "ADCIRC mesh.")
+        parser.add_argument("mesh_path", help="ADCIRC mesh file path.")
+        parser.add_argument("--show-elements", action="store_true",
+                            default=False)
+        parser.add_argument("--vmin", type=float)
+        parser.add_argument("--vmax", type=float)
+        self.args = parser.parse_args()
+
+    def read_mesh(self):
+        self.fort14 = AdcircPy.read_mesh(self.args.mesh_path)
+
+    def plot_mesh(self):
+        self.fort14.make_plot(show=True, elements=self.args.show_elements,
+                              vmin=self.args.vmin, vmax=self.args.vmax)
+
 
 def main():
-  PlotMesh()
+    PlotMesh()
+
 
 if __name__ == "__main__":
-  main()
+    main()
