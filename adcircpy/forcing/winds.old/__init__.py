d974573b010ca3e117abe7282029e558252d9380
jreniel <jreniel@gmail.com>
2020-07-12 14:08:15 -0400

    removed file

--- adcircpy/forcing/winds.old/__init__.py
+++
@@ -1,9 +0,0 @@
-from adcircpy.model.winds.wind_forcing import WindForcing
-from adcircpy.model.winds.best_track import BestTrackForcing
-from adcircpy.model.winds.owi_forcing import OwiForcing
-
-__all__ = [
-    'WindForcing',
-    'BestTrackForcing',
-    'OwiForcing'
-]
