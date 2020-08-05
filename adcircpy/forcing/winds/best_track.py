96559d09a65e126e1676b82c062e2c5959981a6e
Zachary.Burnett <zachary.burnett@noaa.gov>
2020-08-05 08:06:06 -0400

    reorder besttrack

--- adcircpy/forcing/winds/best_track.py
+++ adcircpy/forcing/winds/best_track.py
@@ -6,6 +6,7 @@ import urllib.error
 import urllib.request
 
 # import utm
+from haversine import haversine
 from matplotlib import pyplot
 from matplotlib.transforms import Bbox
 import numpy
@@ -26,85 +27,13 @@ from adcircpy.forcing.winds.base import WindForcing
 class BestTrackForcing(WindForcing):
     def __init__(self, storm_id: str, start_date: datetime = None, end_date: datetime = None,
                  crs: CRS = None):
+        self.start_date = start_date
+        self.end_date = end_date
         self._storm_id = storm_id
-        super().__init__(start_date, end_date, crs)
 
-    def clip_to_bbox(self, bbox: Bbox):
-        """
-        Important: bbox must be expressed in Mercator projection (EPSG:3395)
-        """
-        assert isinstance(bbox, Bbox), f'bbox must be a {Bbox} instance.'
-        bbox_pol = Polygon([
-            [bbox.xmin, bbox.ymin],
-            [bbox.xmax, bbox.ymin],
-            [bbox.xmax, bbox.ymax],
-            [bbox.xmin, bbox.ymax],
-            [bbox.xmin, bbox.ymin]
-        ])
-        _switch = True
-        unique_dates = numpy.unique(self._df['datetime'])
-        for _datetime in unique_dates:
-            records = self._df[self._df['datetime'] == _datetime]
-            radii = records['radius_of_last_closed_isobar'].iloc[0]
-            radii = 1852. * radii  # convert to meters
-            merc = Proj('EPSG:3395')
-            x, y = merc(
-                records['longitude'].iloc[0],
-                records['latitude'].iloc[0])
-            p = Point(x, y)
-            pol = p.buffer(radii)
-            if _switch:
-                if not pol.intersects(bbox_pol):
-                    continue
-                else:
-                    self.start_date = records['datetime'].iloc[0]
-                    _switch = False
-                    continue
-                    # self.start_date =
-            else:
-                if pol.intersects(bbox_pol):
-                    continue
-                else:
-                    self.end_date = records['datetime'].iloc[0]
-                    break
-
-    def plot_trajectory(self, ax: pyplot.Axes = None, show: bool = False, color='k', **kwargs):
-        kwargs.update({'color': color})
-        if ax is None:
-            fig = pyplot.figure()
-            ax = fig.add_subplot(111)
-        for i in range(len(self.speed)):
-            # when dealing with nautical degrees, U is sine and V is cosine.
-            U = self.speed.iloc[i] * numpy.sin(numpy.deg2rad(self.direction.iloc[i]))
-            V = self.speed.iloc[i] * numpy.cos(numpy.deg2rad(self.direction.iloc[i]))
-            ax.quiver(
-                self.longitude.iloc[i], self.latitude.iloc[i], U, V, **kwargs)
-            ax.annotate(
-                self.df['datetime'].iloc[i],
-                (self.longitude.iloc[i], self.latitude.iloc[i])
-            )
-        if show:
-            ax.axis('scaled')
-            pyplot.show()
-
-    def write(self, path: str, overwrite: bool = False):
-        path = pathlib.Path(path)
-        if path.is_file() and not overwrite:
-            raise Exception('Files exist, set overwrite=True to allow overwrite.')
-        with open(path, 'w') as f:
-            f.write(self.fort22)
-
-    @property
-    def NWS(self):
-        try:
-            return self.__nws
-        except AttributeError:
-            return 20
-
-    @NWS.setter
-    def NWS(self, nws: int):
-        assert nws in [19, 20]
-        self.__nws = int(nws)
+        if not isinstance(crs, CRS):
+            crs = CRS.from_user_input(crs)
+        self.__crs = crs
 
     @property
     def storm_id(self):
@@ -135,36 +64,18 @@ class BestTrackForcing(WindForcing):
         self.__atcf = BytesIO(response.read())
 
     @property
-    def _start_date(self):
-        return self.__start_date
+    def crs(self):
+        return self.__crs
 
-    @_start_date.setter
-    def _start_date(self, start_date):
-        if start_date is not None:
-            assert isinstance(start_date, datetime)
-        else:
-            start_date = self._df['datetime'].iloc[0]
-        assert self._df['datetime'].iloc[0] <= start_date < self._df['datetime'].iloc[-1], \
-            f"start_date must be {self._df['datetime'].iloc[0]} <= start_date ({start_date}) < " \
-            f"{self._df['datetime'].iloc[-1]}"
-        self.__start_date = start_date
+    def transform_to(self, crs: CRS):
+        if self.crs != crs:
+            # TODO implement reprojection
+            pass
 
     @property
-    def _end_date(self):
-        return self.__end_date
-
-    @_end_date.setter
-    def _end_date(self, end_date):
-        if end_date is not None:
-            assert isinstance(end_date, datetime)
-        else:
-            end_date = self._df['datetime'].iloc[-1]
-        assert self._df['datetime'].iloc[0] < end_date <= self._df['datetime'].iloc[-1], \
-            f"end_date must be {self._df['datetime'].iloc[0]} <= end_date ({end_date}) <= " \
-            f"{self._df['datetime'].iloc[-1]}"
-        assert end_date > self.start_date, \
-            f"end_date ({end_date}) must be after start_date ({self.start_date})"
-        self.__end_date = end_date
+    def df(self):
+        return self._df[(self._df['datetime'] >= self.start_date) &
+                        (self._df['datetime'] <= self._file_end_date)]
 
     @property
     def _df(self):
@@ -243,6 +154,213 @@ class BestTrackForcing(WindForcing):
             # data = self._transform_coordinates(data)
             self.__df = DataFrame(data=data)
             return self.__df
+
+    @property
+    def name(self):
+        return self.df['name'].value_counts()[:].index.tolist()[0]
+
+    @property
+    def basin(self):
+        return self.df['basin'].iloc[0]
+
+    @property
+    def storm_number(self):
+        return self.df['storm_number'].iloc[0]
+
+    @property
+    def year(self):
+        return self.df['datetime'].iloc[0].year
+
+    @property
+    def datetime(self):
+        return self.df['datetime']
+
+    @property
+    def speed(self):
+        return self.df['speed']
+
+    @property
+    def direction(self):
+        return self.df['direction']
+
+    @property
+    def longitude(self):
+        return self.df['longitude']
+
+    @property
+    def latitude(self):
+        return self.df['latitude']
+
+    @property
+    def start_date(self):
+        return self._start_date
+
+    @start_date.setter
+    def start_date(self, start_date: datetime):
+        self._start_date = start_date
+
+    @property
+    def _start_date(self):
+        return self.__start_date
+
+    @_start_date.setter
+    def _start_date(self, start_date):
+        if start_date is not None:
+            assert isinstance(start_date, datetime)
+        else:
+            start_date = self._df['datetime'].iloc[0]
+        assert self._df['datetime'].iloc[0] <= start_date < self._df['datetime'].iloc[-1], \
+            f"start_date must be {self._df['datetime'].iloc[0]} <= start_date ({start_date}) < " \
+            f"{self._df['datetime'].iloc[-1]}"
+        self.__start_date = start_date
+
+    @property
+    def end_date(self):
+        return self._end_date
+
+    @end_date.setter
+    def end_date(self, end_date: datetime):
+        self._end_date = end_date
+
+    @property
+    def _end_date(self):
+        return self.__end_date
+
+    @_end_date.setter
+    def _end_date(self, end_date):
+        if end_date is not None:
+            assert isinstance(end_date, datetime)
+        else:
+            end_date = self._df['datetime'].iloc[-1]
+        assert self._df['datetime'].iloc[0] < end_date <= self._df['datetime'].iloc[-1], \
+            f"end_date must be {self._df['datetime'].iloc[0]} <= end_date ({end_date}) <= " \
+            f"{self._df['datetime'].iloc[-1]}"
+        assert end_date > self.start_date, \
+            f"end_date ({end_date}) must be after start_date ({self.start_date})"
+        self.__end_date = end_date
+
+    @property
+    def _file_end_date(self):
+        for date in numpy.unique(self._df['datetime']):
+            if date >= self.end_date:
+                return date
+
+    @property
+    def NWS(self):
+        try:
+            return self.__nws
+        except AttributeError:
+            return 20
+
+    @NWS.setter
+    def NWS(self, nws: int):
+        self.__nws = int(nws)
+
+    @staticmethod
+    def _compute_velocity(data: {}) -> {}:
+        """ Output has units of meters per second. """
+
+        merc = Proj('EPSG:3395')
+        x, y = merc(data['longitude'], data['latitude'])
+        t = data['datetime']
+        unique_datetimes = numpy.unique(t)
+        for i, _datetime in enumerate(unique_datetimes):
+            indexes, = numpy.where(numpy.asarray(t) == _datetime)
+            for idx in indexes:
+                if indexes[-1] + 1 < len(t):
+                    dx = haversine((y[idx], x[indexes[-1] + 1]), (y[idx], x[idx]), unit='nmi')
+                    dy = haversine((y[indexes[-1] + 1], x[idx]), (y[idx], x[idx]), unit='nmi')
+                    dt = ((t[indexes[-1] + 1] - t[idx]) / timedelta(hours=1))
+                    vx = numpy.copysign(dx / dt, x[indexes[-1] + 1] - x[idx])
+                    vy = numpy.copysign(dy / dt, y[indexes[-1] + 1] - y[idx])
+                else:
+                    dx = haversine((y[idx], x[indexes[0] - 1]), (y[idx], x[idx]), unit='nmi')
+                    dy = haversine((y[indexes[0] - 1], x[idx]), (y[idx], x[idx]), unit='nmi')
+                    dt = ((t[idx] - t[indexes[0] - 1]) / timedelta(hours=1))
+                    vx = numpy.copysign(dx / dt, x[idx] - x[indexes[0] - 1])
+                    vy = numpy.copysign(dy / dt, y[idx] - y[indexes[0] - 1])
+                speed = numpy.sqrt(dx ** 2 + dy ** 2) / dt
+                bearing = (360. + numpy.rad2deg(numpy.arctan2(vx, vy))) % 360
+                data['speed'].append(int(numpy.around(speed, 0)))
+                data['direction'].append(int(numpy.around(bearing, 0)))
+        return data
+
+    def clip_to_bbox(self, bbox: Bbox):
+        """
+        Important: bbox must be expressed in Mercator projection (EPSG:3395)
+        """
+        assert isinstance(bbox, Bbox), f'bbox must be a {Bbox} instance.'
+        bbox_pol = Polygon([
+            [bbox.xmin, bbox.ymin],
+            [bbox.xmax, bbox.ymin],
+            [bbox.xmax, bbox.ymax],
+            [bbox.xmin, bbox.ymax],
+            [bbox.xmin, bbox.ymin]
+        ])
+        _switch = True
+        unique_dates = numpy.unique(self._df['datetime'])
+        for _datetime in unique_dates:
+            records = self._df[self._df['datetime'] == _datetime]
+            radii = records['radius_of_last_closed_isobar'].iloc[0]
+            radii = 1852. * radii  # convert to meters
+            merc = Proj('EPSG:3395')
+            x, y = merc(
+                records['longitude'].iloc[0],
+                records['latitude'].iloc[0])
+            p = Point(x, y)
+            pol = p.buffer(radii)
+            if _switch:
+                if not pol.intersects(bbox_pol):
+                    continue
+                else:
+                    self.start_date = records['datetime'].iloc[0]
+                    _switch = False
+                    continue
+                    # self.start_date =
+            else:
+                if pol.intersects(bbox_pol):
+                    continue
+                else:
+                    self.end_date = records['datetime'].iloc[0]
+                    break
+
+    def plot_trajectory(self, ax: pyplot.Axes = None, show: bool = False, color='k', **kwargs):
+        kwargs.update({'color': color})
+        if ax is None:
+            fig = pyplot.figure()
+            ax = fig.add_subplot(111)
+        for i in range(len(self.speed)):
+            # when dealing with nautical degrees, U is sine and V is cosine.
+            U = self.speed.iloc[i] * numpy.sin(numpy.deg2rad(self.direction.iloc[i]))
+            V = self.speed.iloc[i] * numpy.cos(numpy.deg2rad(self.direction.iloc[i]))
+            ax.quiver(
+                self.longitude.iloc[i], self.latitude.iloc[i], U, V, **kwargs)
+            ax.annotate(
+                self.df['datetime'].iloc[i],
+                (self.longitude.iloc[i], self.latitude.iloc[i])
+            )
+        if show:
+            ax.axis('scaled')
+            pyplot.show()
+
+    def write(self, path: str, overwrite: bool = False):
+        path = pathlib.Path(path)
+        if path.is_file() and not overwrite:
+            raise Exception('Files exist, set overwrite=True to allow overwrite.')
+        with open(path, 'w') as f:
+            f.write(self.fort22)
+
+    @property
+    def NWS(self):
+        try:
+            return self.__nws
+        except AttributeError:
+            return 20
+
+    @NWS.setter
+    def NWS(self, nws: int):
+        assert nws in [19, 20]
+        self.__nws = int(nws)
 
     @property
     def fort22(self):
