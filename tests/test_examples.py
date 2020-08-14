715859a34ea6f2db89f5062a823efeef02140c41
Zachary.Burnett <zachary.burnett@noaa.gov>
2020-08-14 12:07:39 -0400

    first draft of example test

---
+++ tests/test_examples.py
@@ -0,0 +1,13 @@
+import unittest
+
+from examples.example_3 import main
+
+
+class TestExamples(unittest.TestCase):
+    def test_example_3(self):
+        # TODO implement configuration validation
+        main()
+
+
+if __name__ == '__main__':
+    unittest.main()
