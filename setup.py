e1cba5427b6769e7feee3d2eb6097a341d9ec376
Zachary.Burnett <zachary.burnett@noaa.gov>
2020-11-02 12:56:33 -0500

    move development dependencies to `extras_require`

--- setup.py
+++ setup.py
@@ -17,11 +17,11 @@ setuptools.setup(
     author_email=meta['author_email'],
     description=meta['description'],
     long_description=meta['long_description'],
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     url=meta['url'],
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
-    setup_requires=['dunamai', "setuptools>=41.2"],
+    setup_requires=['dunamai', 'setuptools>=41.2'],
     install_requires=[
         'bs4',
         'eventlet',
@@ -43,37 +43,37 @@ setuptools.setup(
         'tropycal',
         'utm',
         'wget',
-        # test and development dependencies
-        'colored_traceback',
-        'coverage',
-        'coverage-badge',
-        'nose',
-        'rednose'
     ],
+    # test and development dependencies
+    extras_require={
+        'development': [
+            'colored_traceback',
+            'coverage',
+            'coverage-badge',
+            'dunamai',
+            'flake8',
+            'nose',
+            'rednose',
+        ]
+    },
     entry_points={
         'console_scripts': [
-
             # runtime commands
-            "tidal_run=adcircpy.cmd.tidal_run:main",
-            "best_track_run=adcircpy.cmd.best_track_run:main",
-
+            'tidal_run=adcircpy.cmd.tidal_run:main',
+            'best_track_run=adcircpy.cmd.best_track_run:main',
             # hack-a-mesh tools
-            "iter_smooth=adcircpy.cmd.iter_smooth:main",
-
+            'iter_smooth=adcircpy.cmd.iter_smooth:main',
             # Generators
             # 'generate_hindcast=adcircpy._cmd.generate_hindcast:main',
             'best_track_file=adcircpy.cmd.best_track_file:main',
-
             # Plotters
             'plot_mesh=adcircpy.cmd.plot_mesh:main',
             'plot_maxele=adcircpy.cmd.plot_maxele:main',
             'plot_fort61=adcircpy.cmd.plot_fort61:main',
             'fort63=adcircpy.cmd.fort63:main',
-
             # tpxo_install
-            'tpxo_install=adcircpy.forcing.tides.tpxo:install'
-
+            'tpxo_install=adcircpy.forcing.tides.tpxo:install',
         ]
     },
     test_suite='nose.collector',
-    tests_require=['nose'])
+)
