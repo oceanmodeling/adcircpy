e837bfb5b1e9c2fe3ade0b1eac17db0790e82550
Zachary.Burnett <zachary.burnett@noaa.gov>
2020-11-02 13:47:57 -0500

    delete method to fix `flake8` check

--- adcircpy/outputs/fort63.py
+++ adcircpy/outputs/fort63.py
@@ -5,9 +5,3 @@ class Fort63(_ScalarSurfaceOutputTimeseries):
     _filetype = "fort.63"
     _cmap = 'jet'
     _levels = 256
-
-    def subset(self, dst, overwrite=False):
-        zeta = self._ptr['zeta'][:].data
-        zeta_subset = zeta[:, idx_subset]
-        zout = dst.createVariable('zeta', 'f8', ('time', 'node'))
-        zout[:] = zeta_subset[:]
