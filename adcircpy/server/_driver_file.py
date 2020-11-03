6c4d731485959e571b1f274b42142e5f4bb21bd0
zacharyburnettNOAA <52422935+zacharyburnettNOAA@users.noreply.github.com>
2020-11-03 12:22:17 -0500

    driver refactor (#23)
    * refactor of driver file generation in preparation for batch features
    
    * change parameter to specify number of spaces in indent
    
    * add `; then` to `elif` condition
    
    * remove redundant backslashes after pep8 inspection
    
    * add test for driver
    
    * fetch TPXO in test
    
    * setUp and paths
    
    * add print
    
    * remove tidal forcing
    
    * overwrite
    
    * Revert "use `nemspy` to create NEMS configuration (#26)"
    
    This reverts commit b8f24db8051c216d208ea4d12d90a2799f313f95.
    
    * fix tests badge
    
    * fix badges
    
    * Bugfix collection: parse_fort13 and imports
    
    * Made explicit model the default in CLI calls
    
    * refactor of driver file generation in preparation for batch features
    
    * Revert "use `nemspy` to create NEMS configuration (#26)"
    
    This reverts commit b8f24db8051c216d208ea4d12d90a2799f313f95.
    
    * delete method to fix `flake8` check
    
    * Bugfix collection: parse_fort13 and imports
    
    * delete method to fix `flake8` check
    
    * fix reference slurm script
    
    * fix merge break
    
    Co-authored-by: Jaime R Calzada <jreniel@gmail.com>

--- adcircpy/server/_driver_file.py
+++ adcircpy/server/_driver_file.py
@@ -1,4 +1,5 @@
-import pathlib
+import os
+from textwrap import indent
 
 from adcircpy.server.slurm_config import SlurmConfig
 
@@ -8,34 +9,36 @@ class _DriverFile:
     def __init__(self, driver):
         self._driver = driver
 
-    def write(self, path, overwrite=False):
-        with open(pathlib.Path(path), 'w', newline='\n') as f:
-            f.write(self._script)
-        # os.chmod(path, 744)
+    def write(self, path: str, overwrite: bool = False):
+        if not os.path.exists(path) or overwrite:
+            with open(path, 'w', newline='\n') as f:
+                f.write(self._script)
+            # os.chmod(path, 744)
 
     @property
     def _script(self):
-        f = f"{self._shebang}\n"
+        f = f'{self._shebang}\n'
+
         if not isinstance(self._server_config, int):
             f += self._server_config._prefix
         else:
             f += '\nulimit -s unlimited\n' \
                  '\nset -e\n'
 
-        if self._executable.startswith('p') \
-                and isinstance(self._server_config, int):
+        if self._executable.startswith('p') and \
+                isinstance(self._server_config, int):
             if self._server_config > 1:
                 f += f"\nNPROCS={self._nprocs}\n"
 
+        f += '\n'
+
         if self._driver.spinup_time.total_seconds() == 0:
-            f += self._single_phase_run
-
+            f += self._single_phase_run + '\n'
         else:
-            f += self._dual_phase_run
+            f += self._dual_phase_run + '\n'
 
-        f += self._clean_directory
-
-        f += "\nmain\n"
+        f += self._clean_directory + '\n' + \
+             'main\n'
 
         return f
 
@@ -48,159 +51,143 @@ class _DriverFile:
 
     @property
     def _single_phase_run(self):
-        f = r"""
-main()
-{
-  rm -rf work
-  mkdir work
-  cd work
-  ln -sf ../fort.14
-  ln -sf ../fort.13
-  ln -sf ../fort.15 ./fort.15
-"""
+        f = 'rm -rf work\n' \
+            'mkdir work\n' \
+            'cd work\n' \
+            'ln -sf ../fort.14\n' \
+            'ln -sf ../fort.13\n' \
+            'ln -sf ../fort.15 ./fort.15\n'
 
         if self._executable.startswith('p'):
-            f += f"  adcprep --np {self._nprocs} --partmesh\n"
-            f += f"  adcprep --np {self._nprocs} --prepall\n"
-            f += f"  {self._mpi} {self._executable}"
+            f += f'adcprep --np {self._nprocs} --partmesh\n' \
+                 f'adcprep --np {self._nprocs} --prepall\n' \
+                 f'{self._mpi} {self._executable}\n'
         else:
-            f += f"  {self._executable}"
-        f += """
-  clean_directory
-  cd ..
-}
-"""
-        return f
+            f += f'{self._executable}\n'
+
+        f += 'clean_directory\n' + \
+             'cd ..'
+
+        return bash_function('main', f)
 
     @property
     def _dual_phase_run(self):
-        f = ''
-        f += self._bash_main_dual_phase
-        f += self._run_coldstart_phase
-        f += self._run_hotstart_phase
-        return f
+        return self._bash_main_dual_phase + \
+               '\n' + \
+               self._run_coldstart_phase + \
+               '\n' + \
+               self._run_hotstart_phase
 
     @property
     def _bash_main_dual_phase(self):
-        f = """
-main() {
-  SECONDS=0
-  run_coldstart_phase
-  """
-        f += f'if grep -Rq "ERROR: Elevation.gt.ErrorElev, ADCIRC stopping." {self._logfile}; then'
-        f += """
-    duration=$SECONDS
-    echo "ERROR: Elevation.gt.ErrorElev, ADCIRC stopping."
-    echo "Wallclock time: $(($duration / 60)) minutes and $(($duration % 60)) seconds."
-    exit -1
-  else
-    run_hotstart_phase
-    duration=$SECONDS
-    """
-        f += f'if grep -Rq "ERROR: Elevation.gt.ErrorElev, ADCIRC stopping." {self._logfile}; then'
-        f += """
-      echo "ERROR: Elevation.gt.ErrorElev, ADCIRC stopping."
-      echo "Wallclock time: $(($duration / 60)) minutes and $(($duration % 60)) seconds."
-      exit -1
-    fi
-  fi
-  echo "Wallclock time: $(($duration / 60)) minutes and $(($duration % 60)) seconds."
-}
-"""
-        return f
+        error_exit_code = -1
+
+        f = f'SECONDS=0\n' \
+            f'run_coldstart_phase\n'
+
+        f += bash_if_statement(
+            if_condition=f'grep -Rq "ERROR: Elevation.gt.ErrorElev, ADCIRC stopping." {self._logfile}',
+            if_block='duration=$SECONDS\n' \
+                     'echo "ERROR: Elevation.gt.ErrorElev, ADCIRC stopping."\n' \
+                     'echo "Wallclock time: $(($duration / 60)) minutes and $(($duration % 60)) seconds."\n' \
+                     f'exit {error_exit_code}',
+            else_blocks=[
+                'run_hotstart_phase\n'
+                'duration=$SECONDS\n' + \
+                bash_if_statement(
+                    if_condition=f'grep -Rq "ERROR: Elevation.gt.ErrorElev, ADCIRC stopping." {self._logfile}',
+                    if_block='echo "ERROR: Elevation.gt.ErrorElev, ADCIRC stopping."\n' \
+                             'echo "Wallclock time: $(($duration / 60)) minutes and $(($duration % 60)) seconds."\n' \
+                             f'exit {error_exit_code}').strip('\n')
+            ]
+        )
+
+        f += 'echo "Wallclock time: $(($duration / 60)) minutes and $(($duration % 60)) seconds."'
+
+        return bash_function('main', f)
 
     @property
     def _run_coldstart_phase(self):
-        f = """
-run_coldstart_phase() {
-  rm -rf coldstart
-  mkdir coldstart
-  cd coldstart
-  ln -sf ../fort.14
-  ln -sf ../fort.13
-  ln -sf ../fort.15.coldstart ./fort.15
-"""
+        f = 'rm -rf coldstart\n' \
+            'mkdir coldstart\n' \
+            'cd coldstart\n' \
+            'ln -sf ../fort.14\n' \
+            'ln -sf ../fort.13\n' \
+            'ln -sf ../fort.15.coldstart ./fort.15\n'
 
         if self._executable.startswith('p'):
             if isinstance(self._server_config, SlurmConfig):
-                f += "  adcprep --np $SLURM_NTASKS --partmesh\n"
-                f += "  adcprep --np $SLURM_NTASKS --prepall\n"
+                f += 'adcprep --np $SLURM_NTASKS --partmesh\n' \
+                     'adcprep --np $SLURM_NTASKS --prepall\n'
             else:
-                f += f"  adcprep --np {self._nprocs} --partmesh\n"
-                f += f"  adcprep --np {self._nprocs} --prepall\n"
-            f += f"  {self._mpi} {self._executable} "
+                f += f'adcprep --np {self._nprocs} --partmesh\n'
+                f += f'adcprep --np {self._nprocs} --prepall\n'
+            f += f'{self._mpi} {self._executable} '
         else:
-            f += f"  {self._executable} "
+            f += f'{self._executable} '
+
         if not isinstance(self._server_config, SlurmConfig):
-            f += f"2>&1 | tee ../{self._logfile}"
-        f += """
-  clean_directory
-  cd ..
-}
-"""
-        return f
+            f += f'2>&1 | tee ../{self._logfile}'
+
+        f += 'clean_directory\n' \
+             'cd ..'
+
+        return bash_function('run_coldstart_phase', f)
 
     @property
     def _run_hotstart_phase(self):
-        f = """
-run_hotstart_phase() {
-  rm -rf hotstart
-  mkdir hotstart
-  cd hotstart
-  ln -sf ../fort.14
-  ln -sf ../fort.13
-  ln -sf ../fort.15.hotstart ./fort.15
-"""
+        f = 'rm -rf hotstart\n' \
+            'mkdir hotstart\n' \
+            'cd hotstart\n' \
+            'ln -sf ../fort.14\n' \
+            'ln -sf ../fort.13\n' \
+            'ln -sf ../fort.15.hotstart ./fort.15\n'
+
         if self._driver.netcdf is True:
-            f += "  ln -sf ../coldstart/fort.67.nc\n"
+            f += 'ln -sf ../coldstart/fort.67.nc\n'
         else:
-            f += "  ln -sf ../coldstart/fort.67\n"
+            f += 'ln -sf ../coldstart/fort.67\n'
+
         if self._driver.wind_forcing is not None:
             if self._driver.wind_forcing.NWS in [19, 20]:
-                f += "  ln -sf ../fort.22 ./fort.22\n"
-                f += "  aswip\n"
-                f += f"  mv NWS_{self._driver.wind_forcing.NWS}_fort.22 "
-                f += "fort.22\n"
+                f += 'ln -sf ../fort.22 ./fort.22\n' \
+                     'aswip\n' \
+                     f'mv NWS_{self._driver.wind_forcing.NWS}_fort.22 fort.22\n'
             else:
-                msg = f"NWS={self._driver.wind_forcing.NWS}"
+                msg = f'unsupported NWS value {self._driver.wind_forcing.NWS}'
                 raise NotImplementedError(msg)
 
         if self._executable.startswith('p'):
             if isinstance(self._server_config, SlurmConfig):
-                f += "  adcprep --np $SLURM_NTASKS --partmesh\n"
-                f += "  adcprep --np $SLURM_NTASKS --prepall\n"
+                f += 'adcprep --np $SLURM_NTASKS --partmesh\n' \
+                     'adcprep --np $SLURM_NTASKS --prepall\n'
             else:
-                f += f"  adcprep --np {self._nprocs} --partmesh\n"
-                f += f"  adcprep --np {self._nprocs} --prepall\n"
-            f += f"  {self._mpi} {self._executable} "
+                f += f'adcprep --np {self._nprocs} --partmesh\n' \
+                     f'adcprep --np {self._nprocs} --prepall\n'
+            f += f'{self._mpi} {self._executable} '
         else:
-            f += f"  {self._executable} "
+            f += f'{self._executable} '
 
         if not isinstance(self._server_config, SlurmConfig):
-            f += f"2>&1 | tee -a ../{self._logfile}"
+            f += f'2>&1 | tee -a ../{self._logfile}'
+        f += 'clean_directory\n' \
+             'cd ..'
 
-        f += """
-  clean_directory
-  cd ..
-}
-"""
-        return f
+        return bash_function('run_hotstart_phase', f)
 
     @property
     def _clean_directory(self):
-        return """
-clean_directory() {
-  rm -rf PE*
-  rm -rf partmesh.txt
-  rm -rf metis_graph.txt
-  rm -rf fort.13
-  rm -rf fort.14
-  rm -rf fort.15
-  rm -rf fort.16
-  rm -rf fort.80
-  rm -rf fort.68.nc
-}
-"""
+        return bash_function('clean_directory',
+                             '\n'.join(f'rm -rf {member}' for member in
+                                       ['PE*',
+                                        'partmesh.txt',
+                                        'metis_graph.txt',
+                                        'fort.13',
+                                        'fort.14',
+                                        'fort.15',
+                                        'fort.16',
+                                        'fort.80',
+                                        'fort.68.nc']))
 
     @property
     def _logfile(self):
@@ -215,7 +202,6 @@ clean_directory() {
 
     @property
     def _executable(self):
-
         if self._nprocs == 1:
             if self._driver.wave_forcing is not None:
                 return 'adcswan'
@@ -235,7 +221,7 @@ clean_directory() {
         if isinstance(self._server_config, SlurmConfig):
             return self._server_config._launcher
         else:
-            return f"mpiexec -n {self._nprocs}"
+            return f'mpiexec -n {self._nprocs}'
 
     @property
     def _server_config(self):
@@ -247,3 +233,38 @@ clean_directory() {
             return self._server_config
         else:
             return self._server_config.nprocs
+
+
+def bash_if_statement(
+        if_condition: str,
+        if_block: str,
+        else_blocks: [str] = None,
+        indent_string: str = '  '
+) -> str:
+    output = f'if {if_condition}; then\n' + \
+             indent(if_block, indent_string) + '\n'
+
+    if else_blocks is not None:
+        for else_block in else_blocks:
+            if isinstance(else_block, str):
+                output += 'else\n'
+            else:
+                assert len(else_block) == 2, \
+                    f'could not parse else condition / block: {else_block}'
+                output += f'elif {else_block[0]}\n'
+                else_block = else_block[1]
+            output += indent(else_block, indent_string) + '\n'
+
+    output += 'fi\n'
+
+    return output
+
+
+def bash_function(
+        name: str,
+        function_block: str,
+        indent_string: str = '  '
+) -> str:
+    return f'{name}() {{\n' + \
+           indent(function_block, indent_string) + '\n' + \
+           '}\n'
