25613a19963fac733ed96e668428fde567d1cc2b
Zachary.Burnett <zachary.burnett@noaa.gov>
2020-11-06 14:10:03 -0500

    add test for ADCIRC configuration files

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -4,15 +4,16 @@ import pathlib
 import tarfile
 import unittest
 
+import numpy
 import requests
 
-from adcircpy import AdcircMesh, AdcircRun
+from adcircpy import AdcircMesh, AdcircRun, Tides
 from adcircpy.server import SlurmConfig
 from adcircpy.server.driver_file import DriverFile
 
 DATA_DIRECTORY = pathlib.Path(__file__).parent.absolute() / 'data'
-INPUT_DIRECTORY = DATA_DIRECTORY / 'input'
 REFERENCE_DIRECTORY = DATA_DIRECTORY / 'reference'
+INPUT_DIRECTORY = DATA_DIRECTORY / 'input'
 OUTPUT_DIRECTORY = DATA_DIRECTORY / 'output'
 FORT14_FILENAME = INPUT_DIRECTORY / 'fort.14'
 
@@ -24,8 +25,7 @@ class TestAdcircRun(unittest.TestCase):
     def setUp(self):
         # fetch Shinnecock Inlet test data
         if not FORT14_FILENAME.is_file():
-            url = "https://www.dropbox.com/s/1wk91r67cacf132/" \
-                  "NetCDF_shinnecock_inlet.tar.bz2?dl=1"
+            url = 'https://www.dropbox.com/s/1wk91r67cacf132/NetCDF_shinnecock_inlet.tar.bz2?dl=1'
             remote_file = requests.get(url, stream=True)
             input_filename = DATA_DIRECTORY / 'NetCDF_shinnecock_inlet.tar.bz2'
             with open(input_filename, 'wb') as f:
@@ -35,9 +35,9 @@ class TestAdcircRun(unittest.TestCase):
             os.remove(input_filename)
 
     def test_slurm_driver(self):
-        output_directory = OUTPUT_DIRECTORY / 'slurm'
-        reference_directory = REFERENCE_DIRECTORY / 'slurm'
-        os.makedirs(output_directory, exist_ok=True)
+        output_directory = OUTPUT_DIRECTORY / 'test_slurm_driver'
+        reference_directory = REFERENCE_DIRECTORY / 'test_slurm_driver'
+        output_directory.mkdir(parents=True, exist_ok=True)
 
         # open mesh file
         mesh = AdcircMesh.open(FORT14_FILENAME, crs=4326)
@@ -68,6 +68,53 @@ class TestAdcircRun(unittest.TestCase):
         with open(output_directory / 'slurm.job') as generated_file:
             with open(reference_directory / 'slurm.job') as reference_file:
                 assert generated_file.read() == reference_file.read()
+
+    def test_configuration(self):
+        output_directory = OUTPUT_DIRECTORY / 'test_configuration'
+        reference_directory = REFERENCE_DIRECTORY / 'test_configuration'
+        output_directory.mkdir(parents=True, exist_ok=True)
+
+        # open mesh file
+        mesh = AdcircMesh.open(FORT14_FILENAME, crs=4326)
+
+        # let's generate the tau0 factor
+        mesh.generate_tau0()
+
+        # let's also add a mannings to the domain (constant for this example)
+        mesh.mannings_n_at_sea_floor = numpy.full(mesh.values.shape, 0.025)
+
+        # init tidal forcing and setup requests
+        tidal_forcing = Tides()
+        tidal_forcing.use_constituent('M2')
+        tidal_forcing.use_constituent('N2')
+        tidal_forcing.use_constituent('S2')
+        tidal_forcing.use_constituent('K1')
+        tidal_forcing.use_constituent('O1')
+
+        mesh.add_forcing(tidal_forcing)
+
+        # set simulation dates
+        spinup_time = timedelta(days=2)
+        start_date = datetime(2015, 12, 14) + spinup_time
+        end_date = start_date + timedelta(days=3)
+
+        # instantiate AdcircRun object.
+        driver = AdcircRun(
+            mesh,
+            start_date,
+            end_date,
+            spinup_time,
+        )
+
+        driver.write(output_directory, overwrite=True)
+
+        for reference_filename in reference_directory.iterdir():
+            generated_filename = output_directory / reference_filename.name
+            with open(generated_filename) as generated_file:
+                with open(reference_filename) as reference_file:
+                    generated_lines = generated_file.readlines()[1:]
+                    reference_lines = reference_file.readlines()[1:]
+                    assert generated_lines == reference_lines
 
 
 if __name__ == '__main__':
