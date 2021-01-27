157314e3fc7408493f56be91c5e3c705e0308894
zachary.burnett <zachary.burnett@noaa.gov>
2021-01-27 12:50:23 -0500

    use property setter

--- adcircpy/forcing/base.py
+++ adcircpy/forcing/base.py
@@ -3,24 +3,20 @@ from datetime import timedelta
 from os import PathLike
 
 
-class Interval:
-    def __init__(self):
-        self.value = None
-
-    def __set__(self, instance, value: int):
-        if not isinstance(value, timedelta):
-            value = timedelta(seconds=value)
-        self.value = value
-
-    def __get__(self, instance, owner):
-        return self.value
-
-
 class Forcing(ABC):
     def __init__(self, interval: timedelta):
-        self.interval = Interval()
         self.interval = interval
 
     @abstractmethod
     def write(self, directory: PathLike, overwrite: bool = False):
         raise NotImplementedError
+
+    @property
+    def interval(self) -> timedelta:
+        return self.__interval
+
+    @interval.setter
+    def interval(self, interval: timedelta):
+        if not isinstance(interval, timedelta):
+            interval = timedelta(seconds=interval)
+        self.__interval = interval
