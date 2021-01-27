9c9c71df8efbb7a49d14c36d3c695b3458478df5
zachary.burnett <zachary.burnett@noaa.gov>
2021-01-27 12:32:52 -0500

    fix setting interval

--- adcircpy/forcing/base.py
+++ adcircpy/forcing/base.py
@@ -1,8 +1,26 @@
 from abc import ABC, abstractmethod
+from datetime import timedelta
 from os import PathLike
 
 
+class Interval:
+    def __init__(self):
+        self.value = None
+
+    def __set__(self, instance, value: int):
+        if not isinstance(value, timedelta):
+            value = timedelta(seconds=value)
+        self.value = value
+
+    def __get__(self, instance, owner):
+        return self.value
+
+
 class Forcing(ABC):
+    def __init__(self, interval: timedelta):
+        self.interval = Interval()
+        self.interval = interval
+
     @abstractmethod
     def write(self, directory: PathLike, overwrite: bool = False):
         raise NotImplementedError
