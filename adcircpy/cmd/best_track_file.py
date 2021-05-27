e8d4cfe803387133a56bbd6dafa5add3d72c7507
Lint Action <lint-action@samuelmeuli.com>
2021-05-27 16:32:56 +0000

    Fix code style issues with oitnb

--- adcircpy/cmd/best_track_file.py
+++ adcircpy/cmd/best_track_file.py
@@ -8,52 +8,45 @@ from adcircpy.forcing.winds.best_track import BestTrackForcing
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description="generate `fort.22` information from HURDAT2 data"
+        description='generate `fort.22` information from HURDAT2 data'
     )
     parser.add_argument(
-        "storm",
-        help="Can be StormYYYY (eg. Sandy2012), "
-        "a storm id from the HURDAT2 table (ftp://ftp.nhc.noaa.gov/atcf/archive/storm.table), or"
-        "a file path to a track file in HURDAT2 format",
+        'storm',
+        help='Can be StormYYYY (eg. Sandy2012), '
+        'a storm id from the HURDAT2 table (ftp://ftp.nhc.noaa.gov/atcf/archive/storm.table), or'
+        'a file path to a track file in HURDAT2 format',
     )
-    parser.add_argument("--save-path", help="path to which to write fort.22")
-    parser.add_argument("--start-date", help="format is %%Y%%m%%d%%H")
-    parser.add_argument("--end-date", help="format is %%Y%%m%%d%%H")
+    parser.add_argument('--save-path', help='path to which to write fort.22')
+    parser.add_argument('--start-date', help='format is %%Y%%m%%d%%H')
+    parser.add_argument('--end-date', help='format is %%Y%%m%%d%%H')
     parser.add_argument(
-        "--quiet",
-        "-q",
-        action="store_true",
+        '--quiet', '-q', action='store_true', default=False, help='suppress console output',
+    )
+    parser.add_argument(
+        '--plot-track',
+        action='store_true',
         default=False,
-        help="suppress console output",
+        help='show a simple plot of the track',
     )
-    parser.add_argument(
-        "--plot-track",
-        action="store_true",
-        default=False,
-        help="show a simple plot of the track",
-    )
-    parser.add_argument("--nws", help="which NWS forcing option to use")
+    parser.add_argument('--nws', help='which NWS forcing option to use')
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
 
-    if "fort.22" in args.storm:
+    if 'fort.22' in args.storm:
         bt = BestTrackForcing.from_fort22(
-            args.storm,
-            nws=int(args.nws),
-            start_date=args.start_date,
-            end_date=args.end_date,
+            args.storm, nws=int(args.nws), start_date=args.start_date, end_date=args.end_date,
         )
     else:
         bt = BestTrackForcing(
             args.storm,
             nws=int(args.nws) if args.nws is not None else 20,
-            start_date=datetime.strptime(args.start_date, "%Y%m%d%H")
+            start_date=datetime.strptime(args.start_date, '%Y%m%d%H')
             if args.start_date is not None
             else None,
-            end_date=datetime.strptime(args.end_date, "%Y%m%d%H")
+            end_date=datetime.strptime(args.end_date, '%Y%m%d%H')
             if args.end_date is not None
             else None,
         )
@@ -68,9 +61,9 @@ def main():
 
     # save fort22
     if args.save_path is not None:
-        with open(Path(args.save_path), "w") as output_file:
+        with open(Path(args.save_path), 'w') as output_file:
             output_file.write(str(bt))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- tests/test_besttrack.py
+++ tests/test_besttrack.py
@@ -4,28 +4,25 @@ from unittest.mock import patch
 
 from adcircpy.forcing.winds import BestTrackForcing
 
-DATA_DIRECTORY = Path(__file__).parent / "data"
-INPUT_DIRECTORY = DATA_DIRECTORY / "input"
-OUTPUT_DIRECTORY = DATA_DIRECTORY / "output"
-REFERENCE_DIRECTORY = DATA_DIRECTORY / "reference"
+DATA_DIRECTORY = Path(__file__).parent / 'data'
+INPUT_DIRECTORY = DATA_DIRECTORY / 'input'
+OUTPUT_DIRECTORY = DATA_DIRECTORY / 'output'
+REFERENCE_DIRECTORY = DATA_DIRECTORY / 'reference'
 
 
 class TestBestTrack(unittest.TestCase):
     def test_from_fort22(self):
-        input_filename = INPUT_DIRECTORY / "test_besttrack" / "irma2017_fort.22"
-        output_filename = OUTPUT_DIRECTORY / "test_besttrack" / "irma2017_fort.22"
-        reference_filename = REFERENCE_DIRECTORY / "test_besttrack" / "irma2017_fort.22"
+        input_filename = INPUT_DIRECTORY / 'test_besttrack' / 'irma2017_fort.22'
+        output_filename = OUTPUT_DIRECTORY / 'test_besttrack' / 'irma2017_fort.22'
+        reference_filename = REFERENCE_DIRECTORY / 'test_besttrack' / 'irma2017_fort.22'
 
         if not output_filename.parent.exists():
             output_filename.parent.mkdir(parents=True, exist_ok=True)
 
-        best_track = BestTrackForcing.from_fort22(
-                fort22=input_filename,
-                nws=20,
-        )
+        best_track = BestTrackForcing.from_fort22(fort22=input_filename, nws=20,)
 
-        assert best_track.storm_id == "AL112017"
-        assert best_track.name == "IRMA"
+        assert best_track.storm_id == 'AL112017'
+        assert best_track.name == 'IRMA'
 
         best_track.write(output_filename, overwrite=True)
 
@@ -35,17 +32,14 @@ class TestBestTrack(unittest.TestCase):
 
     @patch('matplotlib.pyplot.show')
     def test_from_atcf(self, mock_requests):
-        input_filename = INPUT_DIRECTORY / "test_besttrack" / "florence2018_atcf.trk"
-        output_filename = OUTPUT_DIRECTORY / "test_besttrack" / "florence2018_fort.22"
-        reference_filename = REFERENCE_DIRECTORY / "test_besttrack" / "florence2018_fort.22"
+        input_filename = INPUT_DIRECTORY / 'test_besttrack' / 'florence2018_atcf.trk'
+        output_filename = OUTPUT_DIRECTORY / 'test_besttrack' / 'florence2018_fort.22'
+        reference_filename = REFERENCE_DIRECTORY / 'test_besttrack' / 'florence2018_fort.22'
 
         if not output_filename.parent.exists():
             output_filename.parent.mkdir(parents=True, exist_ok=True)
 
-        best_track = BestTrackForcing.from_atcf_file(
-                atcf=input_filename,
-                nws=8,
-        )
+        best_track = BestTrackForcing.from_atcf_file(atcf=input_filename, nws=8,)
 
         assert best_track.storm_id == 'BT02008'
         assert best_track.name == 'WRT00001'
