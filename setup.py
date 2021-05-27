927ca0e292f4b15732913ca201d4ada8ffd9a2f5
JaimeCalzadaNOAA <70729297+JaimeCalzadaNOAA@users.noreply.github.com>
2021-05-27 19:47:21 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setuptools.setup(
         'paramiko',
         'psutil',
         # 'pygrib', # TODO: make separate install
-        'pyproj',
+        'pyproj>=2.6',
         'geopandas',
         'requests',
         'scipy',
