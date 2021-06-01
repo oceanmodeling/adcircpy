e17c10c183b60b94bc70f0530ed0497f9f1d6843
Keith Roberts <krober@usp.br>
2021-06-01 10:49:16 -0300

    enable plotting the timestep of maximum elevation (#101)
    * enable plotting of timestep of maximum elevation
    
    Co-authored-by: Keith Roberts <krober@usp.edu>
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>

--- adcircpy/cmd/plot_maxele.py
+++ adcircpy/cmd/plot_maxele.py
@@ -2,14 +2,19 @@ import argparse
 
 import matplotlib.pyplot as plt
 
-from adcircpy.outputs import Maxele
+from adcircpy.outputs import Maxele, MaximumElevationTimes
 
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description='Program to see a quick plot of an ADCIRC maxele file.'
+        description='Program to generate a plot of an ADCIRC `maxele` file.'
     )
     parser.add_argument('maxele', help='Path to maxele file.')
+    parser.add_argument(
+        '--plot_timestep_of_maxele',
+        help='Plot the timestep of the maximum elevation value',
+        action='store_true',
+    )
     parser.add_argument(
         '--fort14', help='Path to fort.14 file (required if maxele files is not netcdf).',
     )
@@ -23,7 +28,10 @@ def parse_args():
 
 def main():
     args = parse_args()
-    maxele = Maxele(args.maxele)
+    if args.plot_timestep_of_maxele:
+        maxele = MaximumElevationTimes(args.maxele)
+    else:
+        maxele = Maxele(args.maxele)
     maxele.tricontourf(
         vmin=args.vmin, vmax=args.vmax, cmap=args.cmap, levels=args.levels, cbar=True
     )
