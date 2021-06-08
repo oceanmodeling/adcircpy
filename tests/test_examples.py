aa2a82716f7a229abcdcd0c6d006cf1b0b8fc5fd
zachary.burnett <zachary.burnett@noaa.gov>
2021-06-08 14:25:59 -0400

    fix directories

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -12,7 +12,7 @@ def test_example_1():
     exec(open(EXAMPLES_DIRECTORY / 'example_1.py').read())
 
     reference_directory = REFERENCE_DIRECTORY / 'test_example_1'
-    output_directory = OUTPUT_DIRECTORY / 'test_example_1'
+    output_directory = OUTPUT_DIRECTORY / 'example_1'
 
     check_reference_directory(
         output_directory, reference_directory, skip_lines={'fort.15': [0, -2]}
@@ -23,7 +23,7 @@ def test_example_2():
     exec(open(EXAMPLES_DIRECTORY / 'example_2.py').read())
 
     reference_directory = REFERENCE_DIRECTORY / 'test_example_2'
-    output_directory = OUTPUT_DIRECTORY / 'test_example_2'
+    output_directory = OUTPUT_DIRECTORY / 'example_2'
 
     check_reference_directory(
         output_directory, reference_directory, skip_lines={'fort.15': [0, -2]}
@@ -34,7 +34,7 @@ def test_example_3():
     exec(open(EXAMPLES_DIRECTORY / 'example_3.py').read())
 
     reference_directory = REFERENCE_DIRECTORY / 'test_example_3'
-    output_directory = OUTPUT_DIRECTORY / 'test_example_3'
+    output_directory = OUTPUT_DIRECTORY / 'example_3'
 
     check_reference_directory(
         output_directory, reference_directory, skip_lines={'fort.15': [0, -2]}
@@ -45,7 +45,7 @@ def test_example_4():
     exec(open(EXAMPLES_DIRECTORY / 'example_4.py').read())
 
     reference_directory = REFERENCE_DIRECTORY / 'test_example_4'
-    output_directory = OUTPUT_DIRECTORY / 'test_example_4'
+    output_directory = OUTPUT_DIRECTORY / 'example_4'
 
     check_reference_directory(
         output_directory, reference_directory, skip_lines={'fort.15': [0, -2]}
