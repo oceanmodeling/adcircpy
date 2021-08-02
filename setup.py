1f16f4e3be78d28eb5d82bfb024e578a1002cd42
zachary.burnett <zachary.burnett@noaa.gov>
2021-08-02 09:00:49 -0400

    improvements to dependency installation in setup

--- setup.py
+++ setup.py
@@ -1,69 +1,106 @@
-#!/usr/bin/env python
-import importlib
 import logging
 import os
 from pathlib import Path
+import re
 import subprocess
 import sys
 
-import setuptools
+from setuptools import config, find_packages, setup
 
-BUILT_PACKAGES = {
+DEPENDENCIES = {
+    'appdirs': [],
+    'bs4': [],
+    'eventlet': [],
     'fiona': ['gdal'],
+    'haversine': [],
     'matplotlib': [],
     'netCDF4': [],
     'numpy': [],
-    'pyproj': ['gdal'],
+    'ordered_set': [],
+    'pandas': [],
+    'paramiko': [],
+    'psutil': [],
+    # 'pygrib': [], # TODO: make separate install
+    'pyproj>=2.6': [],
+    'geopandas': [],
+    'requests': [],
     'scipy': [],
-    'shapely': ['gdal'],
+    'seaborn': [],
+    'shapely': [],
+    'tropycal': [],
+    'utm': [],
+    'wget': [],
 }
-is_conda = (Path(sys.prefix) / 'conda-meta').exists()
 
-if is_conda:
-    conda_packages = []
-    for conda_package in BUILT_PACKAGES:
-        try:
-            importlib.import_module(conda_package)
-        except:
-            conda_packages.append(conda_package)
-    if len(conda_packages) > 0:
-        subprocess.check_call(['conda', 'install', '-y', *conda_packages])
 
-if os.name == 'nt':
-    for required_package, pipwin_dependencies in BUILT_PACKAGES.items():
-        try:
-            importlib.import_module(required_package)
-        except:
+def installed_packages() -> [str]:
+    return [
+        re.split('#egg=', re.split('==| @ ', package.decode())[0])[-1].lower()
+        for package in subprocess.check_output(
+                [sys.executable, '-m', 'pip', 'freeze']
+        ).splitlines()
+    ]
+
+
+def missing_packages(dependencies: {str: [str]}) -> {str: [str]}:
+    return {
+        dependency: subdependencies
+        for dependency, subdependencies in dependencies.items()
+        if re.split('<|<=|==|>=|>', dependency)[
+               0].lower() not in installed_packages()
+    }
+
+
+missing_dependencies = missing_packages(DEPENDENCIES)
+
+if (Path(sys.prefix) / 'conda-meta').exists() and len(
+        missing_dependencies) > 0:
+    try:
+        subprocess.check_call(
+                ['conda', 'install', '-y', list(missing_dependencies)])
+    except:
+        for dependency in list(missing_dependencies):
             try:
-                import pipwin
+                subprocess.check_call(['conda', 'install', '-y', dependency])
             except:
-                subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pipwin'])
+                continue
 
-            failed_pipwin_packages = []
-            for pipwin_package in pipwin_dependencies + [required_package]:
-                try:
-                    subprocess.check_call(
-                        [sys.executable, '-m', 'pipwin', 'install', pipwin_package.lower()]
-                    )
-                except subprocess.CalledProcessError:
-                    failed_pipwin_packages.append(pipwin_package)
+    missing_dependencies = missing_packages(DEPENDENCIES)
 
-            if len(failed_pipwin_packages) > 0:
-                raise RuntimeError(
-                    f'failed to download or install non-conda Windows build(s) of {" and ".join(failed_pipwin_packages)}; you can either\n'
-                    '1) install within an Anaconda environment, or\n'
-                    f'2) `pip install <file>.whl`, with `<file>.whl` downloaded from {" and ".join("https://www.lfd.uci.edu/~gohlke/pythonlibs/#" + value.lower() for value in failed_pipwin_packages)} for your Python version'
-                )
+if os.name == 'nt' and len(missing_dependencies) > 0:
+    if 'pipwin' not in installed_packages():
+        subprocess.check_call(
+                [sys.executable, '-m', 'pip', 'install', 'pipwin'])
+    subprocess.check_call([sys.executable, '-m', 'pipwin', 'refresh'])
+
+    for dependency, subdependencies in missing_dependencies.items():
+        failed_pipwin_packages = []
+        for _ in range(1 + len(subdependencies)):
+            for package_name in [dependency] + subdependencies:
+                if package_name in missing_packages(DEPENDENCIES):
+                    try:
+                        subprocess.check_call(
+                                [sys.executable, '-m', 'pipwin', 'install',
+                                 package_name.lower()]
+                        )
+                        if package_name in failed_pipwin_packages:
+                            failed_pipwin_packages.remove(package_name)
+                    except subprocess.CalledProcessError:
+                        failed_pipwin_packages.append(package_name)
+
+            # since we don't know the dependencies here, repeat this process n number of times
+            # (worst case is `O(n)`, where the first package is dependant on all the others)
+            if len(failed_pipwin_packages) == 0:
+                break
+
+    missing_dependencies = missing_packages(DEPENDENCIES)
 
 try:
-    try:
-        from dunamai import Version
-    except ImportError:
-        import subprocess
-        import sys
+    if 'dunamai' not in installed_packages():
+        subprocess.check_call(
+                [sys.executable, '-m', 'pip', 'install', 'dunamai'])
 
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'dunamai'])
-        from dunamai import Version
+    from dunamai import Version
 
     version = Version.from_any_vcs().serialize()
 except RuntimeError as error:
@@ -72,67 +109,43 @@ except RuntimeError as error:
 
 logging.info(f'using version {version}')
 
-metadata = setuptools.config.read_configuration(
-    Path(__file__).parent.absolute() / 'setup.cfg'
-)['metadata']
-setuptools.setup(
-    name=metadata['name'],
-    version=version,
-    author=metadata['author'],
-    author_email=metadata['author_email'],
-    description=metadata['description'],
-    long_description=metadata['long_description'],
-    long_description_content_type='text/markdown',
-    url=metadata['url'],
-    packages=setuptools.find_packages(),
-    python_requires='>=3.6',
-    setup_requires=['dunamai', 'requests', 'setuptools>=41.2'],
-    install_requires=[
-        'appdirs',
-        'bs4',
-        'eventlet',
-        'fiona',
-        'haversine',
-        'matplotlib',
-        'netCDF4',
-        'numpy',
-        'ordered_set',
-        'pandas',
-        'paramiko',
-        'psutil',
-        # 'pygrib', # TODO: make separate install
-        'pyproj>=2.6',
-        'geopandas',
-        'requests',
-        'scipy',
-        'seaborn',
-        'shapely',
-        'tropycal',
-        'utm',
-        'wget',
-    ],
-    # test and development dependencies
-    extras_require={
-        'testing': [
-            'colored_traceback',
-            'FileLock',
-            'pytest',
-            'pytest-mock',
-            'pytest-cov',
-            'pytest-xdist',
-        ],
-        'development': ['dunamai', 'flake8', 'isort', 'oitnb'],
-    },
-    entry_points={
-        'console_scripts': [
-            'tidal_run=adcircpy.cmd.tidal_run:main',
-            'best_track_run=adcircpy.cmd.best_track_run:main',
-            'best_track_file=adcircpy.cmd.best_track_file:main',
-            'plot_mesh=adcircpy.cmd.plot_mesh:main',
-            'plot_maxele=adcircpy.cmd.plot_maxele:main',
-            'plot_fort61=adcircpy.cmd.plot_fort61:main',
-            'fort63=adcircpy.cmd.fort63:main',
-            'tide_gen=adcircpy.cmd.tide_gen:main',
-        ]
-    },
+metadata = config.read_configuration('setup.cfg')['metadata']
+
+setup(
+        name=metadata['name'],
+        version=version,
+        author=metadata['author'],
+        author_email=metadata['author_email'],
+        description=metadata['description'],
+        long_description=metadata['long_description'],
+        long_description_content_type='text/markdown',
+        url=metadata['url'],
+        packages=find_packages(),
+        python_requires='>=3.6',
+        setup_requires=['dunamai', 'requests', 'setuptools>=41.2'],
+        install_requires=list(DEPENDENCIES),
+        # test and development dependencies
+        extras_require={
+            'testing': [
+                'colored_traceback',
+                'FileLock',
+                'pytest',
+                'pytest-mock',
+                'pytest-cov',
+                'pytest-xdist',
+            ],
+            'development': ['dunamai', 'flake8', 'isort', 'oitnb'],
+        },
+        entry_points={
+            'console_scripts': [
+                'tidal_run=adcircpy.cmd.tidal_run:main',
+                'best_track_run=adcircpy.cmd.best_track_run:main',
+                'best_track_file=adcircpy.cmd.best_track_file:main',
+                'plot_mesh=adcircpy.cmd.plot_mesh:main',
+                'plot_maxele=adcircpy.cmd.plot_maxele:main',
+                'plot_fort61=adcircpy.cmd.plot_fort61:main',
+                'fort63=adcircpy.cmd.fort63:main',
+                'tide_gen=adcircpy.cmd.tide_gen:main',
+            ]
+        },
 )
