3dc00d4fd3f1b05cecb73c70fa3b6479a1202760
Zachary Burnett <zachary.burnett@noaa.gov>
2021-08-02 14:36:11 -0400

    build on Windows (#52)
    * if installing on Windows, download precompiled binaries for GDAL and Fiona
    
    * fix scope error
    
    * use `python setup.py install` instead of `pip install .`
    
    * remove Python 3.6 (scipy deprecation)
    
    * separately versioned builds are not needed
    
    * use sudo when installing libraries
    
    * forgot the &&
    
    * add libgeos to install
    
    * add libgeos to install
    
    * add OS conditions to dependency install
    
    * use conditions for linux library install
    
    * fix not error
    
    * explicitly use ubuntu
    
    * fix key hit
    
    * remove gdal version pin
    
    * install to conda if detected
    
    * comma
    
    * update windows build
    
    * update dependencies
    
    * update dependencies
    
    * Fix code style issues with oitnb
    
    * improvements to dependency installation in setup
    
    * try subdependencies first
    
    * check package in subdependencies on every interation
    
    * use `subprocess.run()`
    
    * use `shell=True`
    
    * try `pip install` before `pipwin install`
    
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>

--- setup.py
+++ setup.py
@@ -1,18 +1,76 @@
-#!/usr/bin/env python
+from collections.abc import Mapping
 import logging
-import pathlib
+import os
+from pathlib import Path
+import re
+import subprocess
+import sys
 
-import setuptools
+from setuptools import config, find_packages, setup
+
+DEPENDENCIES = {
+    'appdirs': [],
+    'bs4': [],
+    'eventlet': [],
+    'fiona': ['gdal'],
+    'haversine': [],
+    'matplotlib': [],
+    'netCDF4': [],
+    'numpy': [],
+    'ordered_set': [],
+    'pandas': [],
+    'paramiko': [],
+    'psutil': [],
+    # 'pygrib': [], # TODO: make separate install
+    'pyproj>=2.6': [],
+    'geopandas': [],
+    'requests': [],
+    'scipy': [],
+    'seaborn': [],
+    'shapely': [],
+    'tropycal': [],
+    'utm': [],
+    'wget': [],
+}
+
+
+def installed_packages() -> [str]:
+    return [
+        re.split('#egg=', re.split('==| @ ', package.decode())[0])[-1].lower()
+        for package in subprocess.run(
+            f'{sys.executable} -m pip freeze', shell=True, capture_output=True,
+        ).stdout.splitlines()
+    ]
+
+
+def missing_packages(required_packages: {str: [str]}) -> {str: [str]}:
+    if isinstance(required_packages, Mapping):
+        missing_dependencies = missing_packages(list(required_packages))
+        output = {}
+        for dependency, subdependencies in required_packages.items():
+            missing_subdependencies = missing_packages(subdependencies)
+            if dependency in missing_dependencies or len(missing_subdependencies) > 0:
+                output[dependency] = missing_subdependencies
+        return output
+    else:
+        return [
+            required_package
+            for required_package in required_packages
+            if re.split('<|<=|==|>=|>', required_package)[0].lower()
+            not in installed_packages()
+        ]
+
 
 try:
-    try:
-        from dunamai import Version
-    except ImportError:
-        import subprocess
-        import sys
+    if 'dunamai' not in installed_packages():
+        subprocess.run(
+            f'{sys.executable} -m pip install dunamai',
+            shell=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
 
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'dunamai'])
-        from dunamai import Version
+    from dunamai import Version
 
     version = Version.from_any_vcs().serialize()
 except RuntimeError as error:
@@ -21,10 +79,88 @@ except RuntimeError as error:
 
 logging.info(f'using version {version}')
 
-metadata = setuptools.config.read_configuration(
-    pathlib.Path(__file__).parent.absolute() / 'setup.cfg'
-)['metadata']
-setuptools.setup(
+MISSING_DEPENDENCIES = missing_packages(DEPENDENCIES)
+
+if (Path(sys.prefix) / 'conda-meta').exists() and len(MISSING_DEPENDENCIES) > 0:
+    conda_packages = []
+    for dependency in list(MISSING_DEPENDENCIES):
+        try:
+            process = subprocess.run(
+                f'conda search {dependency}', check=True, shell=True, capture_output=True,
+            )
+            if 'No match found for:' not in process.stdout.decode():
+                conda_packages.append(dependency)
+        except subprocess.CalledProcessError:
+            continue
+
+    try:
+        subprocess.run(
+            f'conda install -y {" ".join(conda_packages)}',
+            check=True,
+            shell=True,
+            stderr=subprocess.DEVNULL,
+        )
+    except subprocess.CalledProcessError:
+        for dependency in conda_packages:
+            try:
+                subprocess.run(
+                    f'conda install -y {dependency}',
+                    check=True,
+                    shell=True,
+                    stderr=subprocess.DEVNULL,
+                )
+            except subprocess.CalledProcessError:
+                continue
+
+    MISSING_DEPENDENCIES = missing_packages(DEPENDENCIES)
+
+if os.name == 'nt' and len(MISSING_DEPENDENCIES) > 0:
+    if 'pipwin' not in installed_packages():
+        subprocess.run(
+            f'{sys.executable} -m pip install pipwin',
+            shell=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+    subprocess.run(f'{sys.executable} -m pipwin refresh', shell=True)
+
+    for dependency, subdependencies in MISSING_DEPENDENCIES.items():
+        failed_pipwin_packages = []
+        for _ in range(1 + len(subdependencies)):
+            for package_name in subdependencies + [dependency]:
+                if dependency in missing_packages(
+                    DEPENDENCIES
+                ) or package_name in missing_packages(subdependencies):
+                    try:
+                        subprocess.run(
+                            f'{sys.executable} -m pip install {package_name.lower()}',
+                            check=True,
+                            shell=True,
+                            stderr=subprocess.DEVNULL,
+                        )
+                        if package_name in failed_pipwin_packages:
+                            failed_pipwin_packages.remove(package_name)
+                    except subprocess.CalledProcessError:
+                        try:
+                            subprocess.run(
+                                f'{sys.executable} -m pipwin install {package_name.lower()}',
+                                check=True,
+                                shell=True,
+                                stderr=subprocess.DEVNULL,
+                            )
+                        except subprocess.CalledProcessError:
+                            failed_pipwin_packages.append(package_name)
+
+            # since we don't know the dependencies here, repeat this process n number of times
+            # (worst case is `O(n)`, where the first package is dependant on all the others)
+            if len(failed_pipwin_packages) == 0:
+                break
+
+    MISSING_DEPENDENCIES = missing_packages(DEPENDENCIES)
+
+metadata = config.read_configuration('setup.cfg')['metadata']
+
+setup(
     name=metadata['name'],
     version=version,
     author=metadata['author'],
@@ -33,33 +169,10 @@ setuptools.setup(
     long_description=metadata['long_description'],
     long_description_content_type='text/markdown',
     url=metadata['url'],
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     python_requires='>=3.6',
-    setup_requires=['dunamai', 'requests', 'setuptools>=41.2'],
-    install_requires=[
-        'appdirs',
-        'bs4',
-        'eventlet',
-        'fiona',
-        'haversine',
-        'matplotlib',
-        'netCDF4',
-        'numpy',
-        'ordered_set',
-        'pandas',
-        'paramiko',
-        'psutil',
-        # 'pygrib', # TODO: make separate install
-        'pyproj>=2.6',
-        'geopandas',
-        'requests',
-        'scipy',
-        'seaborn',
-        'shapely',
-        'tropycal',
-        'utm',
-        'wget',
-    ],
+    setup_requires=['dunamai', 'setuptools>=41.2'],
+    install_requires=list(DEPENDENCIES),
     # test and development dependencies
     extras_require={
         'testing': [
