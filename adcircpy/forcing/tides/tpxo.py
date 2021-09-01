9e6bef5df02e461b2a2fafb13881bab7e72365be
jreniel <jreniel@gmail.com>
2021-09-01 13:14:39 -0400

    Improved interp of near-shore points for TPXO

--- adcircpy/forcing/tides/tpxo.py
+++ adcircpy/forcing/tides/tpxo.py
@@ -9,12 +9,12 @@ from scipy.interpolate import griddata
 
 from adcircpy.forcing.tides.dataset import TidalDataset
 
-TPXO_ENVIRONMENT_VARIABLE = 'TPXO_NCFILE'
-TPXO_FILENAME = 'h_tpxo9.v1.nc'
+TPXO_ENVIRONMENT_VARIABLE = "TPXO_NCFILE"
+TPXO_FILENAME = "h_tpxo9.v1.nc"
 
 
 class TPXO(TidalDataset):
-    DEFAULT_PATH = Path(appdirs.user_data_dir('tpxo')) / TPXO_FILENAME
+    DEFAULT_PATH = Path(appdirs.user_data_dir("tpxo")) / TPXO_FILENAME
 
     def __init__(self, tpxo_dataset_filename: PathLike = None):
         if tpxo_dataset_filename is None:
@@ -30,15 +30,15 @@ class TPXO(TidalDataset):
             self.dataset = Dataset(self.path)
         else:
             raise FileNotFoundError(
-                '\n'.join(
+                "\n".join(
                     [
                         f'No TPXO file found at "{self.path}".',
-                        'New users will need to register and request a copy of '
-                        f'the TPXO9 NetCDF file (specifically `{TPXO_FILENAME}`) '
-                        'from the authors at https://www.tpxo.net.',
-                        'Once you obtain `h_tpxo9.v1.nc`, you can follow one of the following options: ',
+                        "New users will need to register and request a copy of "
+                        f"the TPXO9 NetCDF file (specifically `{TPXO_FILENAME}`) "
+                        "from the authors at https://www.tpxo.net.",
+                        "Once you obtain `h_tpxo9.v1.nc`, you can follow one of the following options: ",
                         f'1) copy or symlink the file to "{self.path}"',
-                        f'2) set the environment variable `{TPXO_ENVIRONMENT_VARIABLE}` to point to the file',
+                        f"2) set the environment variable `{TPXO_ENVIRONMENT_VARIABLE}` to point to the file",
                     ]
                 )
             )
@@ -57,29 +57,29 @@ class TPXO(TidalDataset):
 
     @property
     def x(self) -> np.ndarray:
-        return self.dataset['lon_z'][:, 0].data
+        return self.dataset["lon_z"][:, 0].data
 
     @property
     def y(self) -> np.ndarray:
-        return self.dataset['lat_z'][0, :].data
+        return self.dataset["lat_z"][0, :].data
 
     @property
     def ha(self) -> np.ndarray:
-        return self.dataset['ha'][:]
+        return self.dataset["ha"][:]
 
     @property
     def hp(self) -> np.ndarray:
-        return self.dataset['hp'][:]
+        return self.dataset["hp"][:]
 
     @property
     def constituents(self):
-        if not hasattr(self, '_constituents'):
+        if not hasattr(self, "_constituents"):
             self._constituents = [
                 c.capitalize()
-                for c in self.dataset['con'][:]
-                .astype('|S1')
+                for c in self.dataset["con"][:]
+                .astype("|S1")
                 .tostring()
-                .decode('utf-8')
+                .decode("utf-8")
                 .split()
             ]
         return self._constituents
@@ -95,29 +95,34 @@ class TPXO(TidalDataset):
         self._assert_vertices(vertices)
         constituents = list(map(lambda x: x.lower(), self.constituents))
         constituent = constituents.index(constituent.lower())
-        array = tpxo_array[constituent, :, :].flatten()
-        _x = np.asarray([x + 360.0 for x in vertices[:, 0] if x < 0]).flatten()
-        _y = vertices[:, 1].flatten()
-        x, y = np.meshgrid(self.x, self.y, indexing='ij')
-        x = x.flatten()
-        y = y.flatten()
+        zi = tpxo_array[constituent, :, :].flatten()
+        xo = np.asarray([x + 360.0 for x in vertices[:, 0] if x < 0]).flatten()
+        yo = vertices[:, 1].flatten()
+        xi, yi = np.meshgrid(self.x, self.y, indexing="ij")
+        xi = xi.flatten()
+        yi = yi.flatten()
         dx = np.mean(np.diff(self.x))
         dy = np.mean(np.diff(self.y))
-
-        # buffer the bbox by 2 difference units
-        _idx = np.where(
-            np.logical_and(
-                np.logical_and(x >= np.min(_x) - 2 * dx, x <= np.max(_x) + 2 * dx),
-                np.logical_and(y >= np.min(_y) - 2 * dy, y <= np.max(_y) + 2 * dy),
-            )
+        # buffer window by 2 pixel units
+        mask1 = np.logical_and(
+            np.logical_and(xi >= np.min(xo) - 2 * dx, xi <= np.max(xo) + 2 * dx),
+            np.logical_and(yi >= np.min(yo) - 2 * dy, yi <= np.max(yo) + 2 * dy),
         )
-
-        # "method" can be 'spline' or any string accepted by griddata()'s method kwarg.
+        # remove junk values from input array
+        mask2 = np.ma.masked_where(zi != 0.0, zi)
+        iidx = np.where(np.logical_and(mask1, mask2))
         values = griddata(
-            (x[_idx], y[_idx]), array[_idx], (_x, _y), method='linear', fill_value=np.nan,
+            (xi[iidx], yi[iidx]),
+            zi[iidx],
+            (xo, yo),
+            method="linear",
+            fill_value=np.nan,
         )
         nan_idxs = np.where(np.isnan(values))
         values[nan_idxs] = griddata(
-            (x[_idx], y[_idx]), array[_idx], (_x[nan_idxs], _y[nan_idxs]), method='nearest',
+            (xi[iidx], i[iidx]),
+            zi[iidx],
+            (xo[nan_idxs], yo[nan_idxs]),
+            method="nearest",
         )
         return values
