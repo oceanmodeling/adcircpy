0a4f0efec759664e4e384e1242f3b8224103e058
Zachary Burnett <zachary.burnett@noaa.gov>
2021-11-11 11:18:40 -0500

    move `VortexForcing` into `ModelForcings` (#139)
    * move `VortexForcing` into `ModelForcings`
    
    * order dependencies alphabetically
    
    * import from `modelforcings`
    
    * this was incorrect syntax, my bad

--- tests/test_besttrack.py
+++ tests/test_besttrack.py
@@ -1,11 +1,6 @@
 # ! /usr/bin/env python
-from copy import copy
 
-from dateutil.parser import parse as parse_date
-import pytest
-from pytest_socket import SocketBlockedError
-
-from adcircpy.forcing.winds.best_track import BestTrackForcing, VortexForcing
+from adcircpy.forcing.winds.best_track import BestTrackForcing
 from tests import (
     check_reference_directory,
     INPUT_DIRECTORY,
@@ -78,72 +73,5 @@ def test_recompute_velocity():
     best_track.dataframe['longitude'][5] -= 0.1
 
     best_track.write(output_directory / 'irma2017_fort.22', overwrite=True)
-
-    check_reference_directory(output_directory, reference_directory)
-
-
-def test_vortex_types():
-    output_directory = OUTPUT_DIRECTORY / 'test_vortex_types'
-    reference_directory = REFERENCE_DIRECTORY / 'test_vortex_types'
-
-    if not output_directory.exists():
-        output_directory.mkdir(parents=True, exist_ok=True)
-
-    file_decks = {
-        'a': {
-            'start_date': parse_date('2018-09-11 06:00'),
-            'end_date': None,
-            'record_types': ['OFCL', 'HWRF', 'HMON', 'CARQ'],
-        },
-        'b': {
-            'start_date': parse_date('2018-09-11 06:00'),
-            'end_date': parse_date('2018-09-18 06:00'),
-            'record_types': ['BEST'],
-        },
-    }
-
-    for file_deck, values in file_decks.items():
-        for record_type in values['record_types']:
-            cyclone = VortexForcing(
-                'al062018',
-                start_date=values['start_date'],
-                end_date=values['end_date'],
-                file_deck=file_deck,
-                record_type=record_type,
-            )
-
-            cyclone.write(
-                output_directory / f'{file_deck}-deck_{record_type}.txt', overwrite=True
-            )
-
-    check_reference_directory(output_directory, reference_directory)
-
-
-@pytest.mark.disable_socket
-def test_no_internet():
-    input_directory = INPUT_DIRECTORY / 'test_no_internet'
-    output_directory = OUTPUT_DIRECTORY / 'test_no_internet'
-    reference_directory = REFERENCE_DIRECTORY / 'test_no_internet'
-
-    if not output_directory.exists():
-        output_directory.mkdir(parents=True, exist_ok=True)
-
-    with pytest.raises((ConnectionError, SocketBlockedError)):
-        VortexForcing(storm='florence2018')
-
-    with pytest.raises((ConnectionError, SocketBlockedError)):
-        VortexForcing(storm='al062018', start_date='20180911', end_date=None)
-
-    vortex_1 = VortexForcing.from_fort22(input_directory / 'fort.22')
-    vortex_1.write(output_directory / 'vortex_1.22', overwrite=True)
-
-    vortex_2 = VortexForcing.from_fort22(vortex_1.filename)
-    vortex_2.write(output_directory / 'vortex_2.22', overwrite=True)
-
-    vortex_3 = copy(vortex_1)
-    vortex_3.write(output_directory / 'vortex_3.22', overwrite=True)
-
-    assert vortex_1 == vortex_2
-    assert vortex_1 == vortex_3
 
     check_reference_directory(output_directory, reference_directory)
