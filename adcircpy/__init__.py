865bb4bb7607f78da6b0fa11bbb0dfe19d672858
zacharyburnettNOAA <zachary.burnett@noaa.gov>
2022-01-18 16:08:22 -0500

    remove call to `importlib` for python 3.6

--- adcircpy/__init__.py
+++ adcircpy/__init__.py
@@ -1,5 +1,3 @@
-from importlib import util
-
 import matplotlib as mpl
 from pandas.plotting import register_matplotlib_converters
 
@@ -21,7 +19,9 @@ __all__ = [
 mpl.rcParams['agg.path.chunksize'] = 10000
 register_matplotlib_converters()
 
-if util.find_spec('colored_traceback') is not None:
+try:
     import colored_traceback
 
     colored_traceback.add_hook(always=True)
+except ImportError:
+    pass
