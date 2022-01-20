541dfa8676547939035416566af48b791356478c
zacharyburnettNOAA <zachary.burnett@noaa.gov>
2022-01-20 10:10:14 -0500

    update reference files

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,3 +1,5 @@
+import os
+
 from tests import (
     check_reference_directory,
     DATA_DIRECTORY,
@@ -9,10 +11,13 @@ EXAMPLES_DIRECTORY = DATA_DIRECTORY.parent.parent / 'examples'
 
 
 def test_example_1():
-    exec(open(EXAMPLES_DIRECTORY / 'example_1.py').read())
-
     reference_directory = REFERENCE_DIRECTORY / 'test_example_1'
     output_directory = OUTPUT_DIRECTORY / 'example_1'
+
+    for filename in output_directory.iterdir():
+        os.remove(filename)
+
+    exec(open(EXAMPLES_DIRECTORY / 'example_1.py').read())
 
     check_reference_directory(
         output_directory, reference_directory, skip_lines={'fort.15': [0, -2]}
@@ -20,10 +25,13 @@ def test_example_1():
 
 
 def test_example_2():
-    exec(open(EXAMPLES_DIRECTORY / 'example_2.py').read())
-
     reference_directory = REFERENCE_DIRECTORY / 'test_example_2'
     output_directory = OUTPUT_DIRECTORY / 'example_2'
+
+    for filename in output_directory.iterdir():
+        os.remove(filename)
+
+    exec(open(EXAMPLES_DIRECTORY / 'example_2.py').read())
 
     check_reference_directory(
         output_directory, reference_directory, skip_lines={'fort.15': [0, -2]}
@@ -31,10 +39,13 @@ def test_example_2():
 
 
 def test_example_3():
-    exec(open(EXAMPLES_DIRECTORY / 'example_3.py').read())
-
     reference_directory = REFERENCE_DIRECTORY / 'test_example_3'
     output_directory = OUTPUT_DIRECTORY / 'example_3'
+
+    for filename in output_directory.iterdir():
+        os.remove(filename)
+
+    exec(open(EXAMPLES_DIRECTORY / 'example_3.py').read())
 
     check_reference_directory(
         output_directory, reference_directory, skip_lines={'fort.15': [0, -2]}
@@ -42,11 +53,16 @@ def test_example_3():
 
 
 def test_example_4():
-    exec(open(EXAMPLES_DIRECTORY / 'example_4.py').read())
-
     reference_directory = REFERENCE_DIRECTORY / 'test_example_4'
     output_directory = OUTPUT_DIRECTORY / 'example_4'
 
+    for filename in output_directory.iterdir():
+        os.remove(filename)
+
+    exec(open(EXAMPLES_DIRECTORY / 'example_4.py').read())
+
     check_reference_directory(
-        output_directory, reference_directory, skip_lines={'fort.15': [0, -2]}
+        output_directory,
+        reference_directory,
+        skip_lines={'fort.15.coldstart': [0, -2], 'fort.15.hotstart': [0, -3]},
     )
