b5e41be0d6e9d6ffc9a605ff6122a35a7bf66854
Zachary Burnett <zachary.burnett@noaa.gov>
2022-03-10 17:20:45 -0500

    use new `StormEvents` API (#148)
    * use new `StormEvents` API
    
    * pin above version

--- tests/test_besttrack.py
+++ tests/test_besttrack.py
@@ -7,8 +7,8 @@ from tests import INPUT_DIRECTORY
 def test_plot_besttrack(mocker):
     input_directory = INPUT_DIRECTORY / 'test_plot_besttrack'
 
-    best_track = BestTrackForcing.from_atcf_file(
-        atcf=input_directory / 'florence2018_atcf.trk', nws=8,
+    best_track = BestTrackForcing.from_fort22(
+        input_directory / 'florence2018_atcf.trk', nws=8,
     )
 
     mocker.patch('matplotlib.pyplot.show')
