89bbf45e6f426db1f0e77d63681c6a9b2d9d7341
SorooshMani-NOAA <soroosh.mani@noaa.gov>
2022-10-14 11:45:18 -0400

    fixup6

--- tests/test_github_action.py
+++ tests/test_github_action.py
@@ -4,15 +4,15 @@ import xarray as xr
 
 url = 'https://icdc.cen.uni-hamburg.de/thredds/dodsC/ftpthredds/hamtide//m2.hamtide11a.nc'
 
-test_get_pydap():
+def test_get_pydap():
     ds = open_url(url)
     print("OPENDAP", ds)
 
-test_open_w_nc4():
+def test_open_w_nc4():
     ds = Dataset(url)
     print("NETCDF4", ds)
 
-test_open_w_xr():
+def test_open_w_xr():
     ds = xr.open_dataset(url)
     print("XARRAY", ds)
 
